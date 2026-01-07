b28d8310ea27d6c179e955a91e6b800ad3de1371
Mohandar-ai <mohandar@gmail.com>
2026-01-06 21:35:17 -0500

    updated to use connection string
    Created SQL Script, updated init file to use new connection schema and also implemented search with similarity

--- smart-doc-qa/__init__.py
+++ smart-doc-qa/__init__.py
@@ -1,15 +1,29 @@
+import src
 import src.data_platform.document_processing.loaders.directory_loader as directory_loader
 from src.data_platform.retrieval.vector_search import search_documents
 from src.data_platform.storage.start_PostGres import ensure_postgres_started
-
+from src.data_platform.retrieval.search_similarity import search_pgvector
+#from src.data_platform.test_documents 
 # Run
 if __name__ == "__main__":
     print("🚀 Starting PostgreSQL database...")
-    if ensure_postgres_started():
-        print("✅ PostgreSQL is running.")
-    print("🚀 Starting document ingestion and vectorization...")
-    directory_loader.Load_and_vectorize("/Users/karthikkumarthirugnanam/AI Fellowship/5 PDF")
+    # if ensure_postgres_started():
+    #     print("✅ PostgreSQL is running.")
+    # print("🚀 Starting document ingestion and vectorization...")
+    #directory_loader.Load_and_vectorize("/Users/karthikkumarthirugnanam/AI Fellowship/5 PDF")
+   # directory_loader.Load_and_vectorize("C:\Mohandar\Capstone-Project\GenAI-Capstone-Project\smart-doc-qa\src\data_platform\test_documents")
     print("✅ Document ingestion and vectorization completed.")
     # Test search
     print("🔍 Searching documents...")
-    print(search_documents("Summarize the document"))
+    # print(search_documents("Summarize the document"))
+    # print("++++++++++++++++++++✅ Search 1 completed.++++++++++++++++++++")
+    ########################################### Mohandar ##################################
+    ###Following search query is used to provide similarity based search results from the PGVector database it will return top 3 results with minimum similarity of 0.5
+     
+    #search_query = "What is Synthetic Data sumarize synthetic data techniques?"
+    search_query = "Summarize the document NLP and LLM"
+    hits = search_pgvector(search_query, top_k=5, min_similarity=0.1)
+    for row in hits:
+        print(f"[{row['similarity']:.4f}] \nFileName {row['filename']}\nchunk: {row['chunk']}")
+        print("---------------**********************************************************************---------------")
+    print("++++++++++++++++++++✅ Search 2 completed.++++++++++++++++++++")
--- smart-doc-qa/config/database.py
+++ smart-doc-qa/config/database.py
@@ -1,4 +1,27 @@
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 DATA_DIR = Path("/opt/homebrew/var/postgresql@14")   # your PGDATA
 LOG_FILE = DATA_DIR / "server.log"
+
+class Settings:
+    DB_HOST = os.getenv("DB_HOST")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DATABASE_URL = (
+        f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"
+        f"@{DB_HOST}:/{DB_NAME}"
+    )
+    
+settings = Settings()
+# 2. Database connection parameters
+# conn_params = {
+#         "host": "localhost",
+#         "database": "GenAI-Capstone",
+#         "user": "postgres",
+#         "password": "Lenovo"
+# }
+
+##conn = psycopg2.connect(dbname="postgres", user="karthikkumarthirugnanam", host="localhost")
