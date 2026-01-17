e03174d8aead6c6d6edbab46bf6e5a352e124d1f
Balaji Calluru <balaji.calluru@gmail.com>
2026-01-17 14:38:44 -0600

    Setting up integrations

--- smart-doc-qa/config/database.py
+++ smart-doc-qa/config/database.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
 DATA_DIR = Path("/opt/homebrew/var/postgresql@18")   # your PGDATA
 LOG_FILE = DATA_DIR / "server.log"
-
 class Settings:
     DB_HOST = os.getenv("DB_HOST")
     DB_NAME = os.getenv("DB_NAME")
@@ -14,7 +15,19 @@ class Settings:
         f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"
         f"@{DB_HOST}:/{DB_NAME}"
     )
+
+    # OpenAI
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     
+    # Pinecone
+    PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+    PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
+    PINECONE_INDEX = os.getenv("PINECONE_INDEX", "documents")
+    
+    # API (TO ADD)
+    API_PORT = int(os.getenv("API_PORT", 8888))
+    API_HOST = os.getenv("API_HOST", "localhost")
+
 settings = Settings()
 # 2. Database connection parameters
 # conn_params = {
@@ -25,3 +38,4 @@ settings = Settings()
 # }
 
 ##conn = psycopg2.connect(dbname="postgres", user="karthikkumarthirugnanam", host="localhost")
+
